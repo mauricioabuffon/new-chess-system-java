0a67d8af95b1fba82e4d7e03bafa761c279fca78
Mauricio Antonio Buffon <mauricioabuffon@gmail.com>
2020-11-16 15:30:57 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Piece {
+	protected Position position;
+	private Board board;
+
+	public Piece(Position position, Board board) {
+		this.position = position;
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+}
