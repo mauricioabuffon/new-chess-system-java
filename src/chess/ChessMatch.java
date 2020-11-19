d11249cfe119a2acb50a41c8164be8a3bf4504af
Mauricio Antonio Buffon <mauricioabuffon@gmail.com>
2020-11-18 21:26:19 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -38,6 +38,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece in this position: " + position);
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no move to this piece");
+		}
 	}
 
 	private Piece makeMove(Position source, Position target) {
